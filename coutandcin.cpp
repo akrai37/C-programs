ee894fbbdbd8ca7a3116f840857b8f47a844e5e4
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-04-22 23:35:54 +0530

    Add files via upload

---
+++ coutandcin.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main ()
+{
+
+int age;
+
+float average;
+
+cout << "enter the age and average " << endl;
+
+cin >> age >> average;
+
+cout << "the age is "<< age << endl << "the average is " << average ;
+
+return 0 ;
+
+
+
+}
---
+++ forloops.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main ()
+{
+   int i;
+
+   for( i=1 ; i<=30 ; i++)
+   {
+       cout << "for looping working " << endl ;
+
+   }
+
+    return 0;
+
+}
