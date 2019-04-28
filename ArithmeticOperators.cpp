6d68a8483540389d2fb92f8261455b48a572cef3
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-04-28 21:08:07 +0530

    CPP ArithmeticOperators

---
+++ ArithmeticOperators.cpp
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+int num1 , num2 ;
+
+cout << "enter the two numbers" << endl ;
+
+cin >> num1 >> num2 ;
+
+cout << " addition of two numbers is " << num1 + num2 ;
+
+
+cout << " subtraction of two numbers is " << num1 - num2 ;
+
+
+cout << " multiplication of two numbers is " << num1 * num2 ;
+
+
+cout << " division of two numbers is " << num1 / num2 ;
+
+return 0 ;
+
+}
