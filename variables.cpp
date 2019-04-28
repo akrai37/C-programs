7af7cd167451d00a62272f35c60e0b92e6b5e9b7
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-04-28 21:06:27 +0530

    variables
    if just printing variable of int or float witout any value then it will print "garbage value". It is name of the memory location

---
+++ variables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+    int age;
+    float average;
+    char ak;
+    cout<<age<<endl<<average<<endl<<ak;
+    return 0;
+}
