d3ccc2a25d79210436b749d9dc19a1fa2358b283
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-04-29 20:29:25 +0530

    Add files via upload
    use of while loop in cpp

---
+++ whileloops.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std ;
+
+int main()
+{
+    int i ;
+
+    i=2;
+    while(  i<=30){
+
+       cout << "eat speep code repeat" << endl;
+
+       i++;
+    }
+
+    return 0;
+
+}
