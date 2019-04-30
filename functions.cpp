eb5d21e2f641577b61abbbe1bfeb84c42e49f2de
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-04-30 21:49:22 +0530

    Cpp functions
    if function is defined after main fn , then we have to create a Prototype which is declaring fn before main with the datatype of parameters . semicolon is must for prototype.

---
+++ functions.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+void display();
+
+int main()
+
+{
+
+   display();
+   display();
+
+    return 0;
+
+}
+
+
+void display() {
+
+    cout <<"welcome functions";
+}
