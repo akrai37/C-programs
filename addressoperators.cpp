300b334e2110ce37206d94484f04aa4aa80b4633
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-03 21:48:24 +0530

    &_address_operator
    "&" is the address operator . it stores the address of the variable that has stored some values.

---
+++ addressoperators.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+    int age=12,weight=50;
+
+    cout << age << endl;
+    cout << &age << endl;
+
+    cout << weight << endl;
+    cout << &weight << endl;
+
+    return 0;
+}
