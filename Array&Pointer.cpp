d70e06c1c0b856c29e137c565a3994fd6ecd1e47
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-04 23:53:30 +0530

    ArrayElements_by_pointers
    we can get or access the array values from two methods. through index as well as pointers. for index, syntax-a[any_number_in_array] and for pointers , syntax- *(a+any_number_in_array)

---
+++ Array&Pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(){
+
+int a[5]={23,56,43,45,19};
+
+cout<<a<<endl;
+cout<<a[2]<<endl;
+cout<<*(a+2)<<endl;
+cout<<*(a+3)<<endl;
+
+return 0;
+}
