d125c63471dedfbb1304dd1d40123f98b3841586
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-05 23:36:59 +0530

    Structure variables&definition
    -Structure is the collection of different  elements of differnt variables. unlike array which is the collection of similar elements of same data type. we define the variable of the structure in the main fn (or any other fn)  and then through that variable we can access and initialize the variables of structure . syntax-struct student{ }  , ex- student stud ; stud.name= "john"; 
    -in other words , we can say that structure is the modified version of array since all the values will  be stored in the name of SAME variable name but they can  be of differnt datatype.

---
+++ Structures.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+struct student{
+
+int roll;
+char gender;
+
+}john,ali; //john,ali are both global STRUCTURE variable and can be used anywhere in the program .
+
+int main(){
+
+student status;//status here is the local structure variable and can only be used in the main fn.
+
+status.roll=45;
+status.gender='m';
+
+john.roll=57;
+john.gender='m';
+
+cout<<status.roll<<endl;
+cout<<john.gender<<endl;
+
+return 0;
+}
