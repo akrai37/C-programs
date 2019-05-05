1aca34c2363a7002df7e486aec8ea2b988d37099
ANKUSH RAI <49373485+akrai37@users.noreply.github.com>
2019-05-05 23:49:03 +0530

    Add files via upload

---
+++ passingStruct_fn.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+
+
+struct student{
+
+int roll; // member
+char gender;//member
+int age;//member
+
+};
+
+void display(student s);
+void show(student *s);
+
+int main(){
+
+student dab= {2414,'m',22};//one more way to declare the values of STRCTURE member
+
+
+ display(dab);
+
+ show(&dab);
+
+
+ display(dab);
+
+ show(&dab);
+
+}
+
+void display(student s){
+
+cout<<s.roll<<endl;
+cout<<s.gender<<endl;
+cout<<s.age<<endl;
+
+s.age=32;
+
+}
+
+
+void show(student *s){
+
+cout<<s->roll<<endl;
+cout<<s->gender<<endl;
+cout<<s->age<<endl;
+
+s->age=54;
+
+}
+
+
+
